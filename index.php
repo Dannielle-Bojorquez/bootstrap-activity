04bb13a1beec3d8d0c049bd25af4e1a331080596
Dannielle Bojorquez <dbojorquez@cnm.edu>
2017-08-07 12:57:09 -0600

    adding header

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title></title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity=
+		"sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+		integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r"
+		crossorigin="anonymous">
+		<!-- Font Awesome -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css
+" rel="stylesheet">
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<!-- jQuery - required for Bootstrap Components -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+				  integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+				  crossorigin="anonymous"></script>
+		<title>Bootstrap Challenge</title>
+
+	</head>
+	<body>
+<nav class="navbar navbar-inverse">
+	<div class="navbar-header">
+		<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#main-menu">
+			<span class="sr-only">main menu</span>
+			<span class="glyphicon-menu-hamburger"></span>
+		</button>
+		<a class="navbar-brand" href="#">Title</a>
+	</div>
+	<div class="collapse navbar-collapse" id="main menu">
+		<ul class="nav navbar-nav navbar-right">
+			<li><a href="#">Link1</a></li>
+			<li><a href="#">Link2</a></li>
+			<li><a href="#">Link3</a></li>
+			<li><a href="#">Link4</a></li>
+		</ul>
+	</div>
+</nav>
+				</div>
+			</div>
+		</div>
+		<div class="jumbotron welcome-block">
+			<h1>Hello, World!</h1>
+			<p class="lead">This is a simple Hero unit, a simple jumbotron-style component for calling extra <br>attention to featured content and information</p>
+			<p class="subnav">
+				<a class="btn btn-info" href="getting-started.html" role="button">Learn More</a>
+			</p>
+		</div>
+		</nav>
+		<link rel="stylesheet" href="stylesheet.css">
+
+	</body>
+</html>
